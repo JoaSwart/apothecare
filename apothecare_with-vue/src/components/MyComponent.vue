6e7d179cfa76103b755c3316c55e17cb250243d1
ERIC <ericcarcaspin@gmail.com>
2025-10-15 10:54:15 +0200

    Vue.js-implementatie

---
+++ apothecare_with-vue/src/components/MyComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="my-component">
+        <h1>{{ message }}</h1>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'MyComponent',
+    data() {
+        return {
+            message: 'Hello, Vue!'
+        };
+    }
+};
+
+</script>
