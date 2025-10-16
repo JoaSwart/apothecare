0158cf014371ee14ef7cdfe893a9a6d95a8a49dd
Julia Heemskerk <juliaheemskerk@icloud.com>
2025-10-16 09:55:53 +0200

    Footer in vue

---
+++ apothecare_with-vue/src/components/Footer.vue
@@ -0,0 +1,63 @@
+<template>
+  <footer class="site-footer">
+    <div class="footer-col">
+      <p class="footer-title">&copy; 2025 Apothecare</p>
+      <p class="footer-p">
+        Uw betrouwbare online drogisterij & apotheek voor medicijnen, vitamines,
+        verzorging en beauty producten. Snel geleverd aan huis.
+      </p>
+    </div>
+
+    <div class="footer-col">
+      <p class="footer-title">Voorwaarden</p>
+      <a href="#" class="footer-p link-reset">Producten</a>
+    </div>
+
+    <div class="footer-col">
+      <p class="footer-title">020 - 123 4567</p>
+      <p class="footer-p">info@apothecare.nl</p>
+      <p class="footer-p">Amsterdam, Nederland</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "SiteFooter",
+};
+</script>
+
+<style scoped>
+footer {
+  background-color: #ffffff;
+  display: flex;
+  color: #000000;
+  text-align: left;
+  padding: 20px 0;
+  position: relative;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  bottom: 0;
+  width: 100vw;
+  border-top: 1px solid #e0e0e0;
+  font-family: Arial, sans-serif;
+}
+
+footer div {
+  width: 30vw;
+  padding: 0 40px;
+}
+
+.footer-title {
+  font-size: 18px;
+  margin-bottom: 15px;
+  color: black;
+  padding: 5px;
+}
+
+.footer-p {
+  font-size: 16px;
+  color: #52555e;
+  padding: 5px;
+}
+</style>
